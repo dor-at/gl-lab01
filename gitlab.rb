e94afaa233c45bfcfa3ecf5c027b43c889c1c074
dor-at <Dor.Attar@bynet.co.il>
2025-12-07 10:43:42 +0200

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -4,17 +4,17 @@
 
 postgresql['enable'] = false
 gitlab_rails['db_adapter'] = 'postgresql'
-gitlab_rails['db_database'] = 'postgre'
+gitlab_rails['db_database'] = 'postgres'
 gitlab_rails['db_encoding'] = 'unicode'
-gitlab_rails['db_host'] = 'gl-lab01-db-instance.c98wptimekfu.eu-west-1.rds.amazonaws.com'
+gitlab_rails['db_host'] = '<host>'
 gitlab_rails['db_port'] = 5432
-gitlab_rails['db_password'] = 'ukuvfrbk3w3wWQBQfuNHRLaP3AkW'
+gitlab_rails['db_password'] = '<password>'
 postgresql['auto_restart_on_version_change'] = false
 
 redis['enable'] = false
-gitlab_rails['redis_host'] = 'master.gle6ihehaee8fov.0lpyn0.euw1.cache.amazonaws.com'
+gitlab_rails['redis_host'] = '<host>'
 gitlab_rails['redis_port'] = '6379'
-gitlab_rails['redis_password'] = 'X35Q69xxNhOse9BK'
+gitlab_rails['redis_password'] = '<token>'
 gitlab_rails['redis_ssl'] = false
 
 gitlab_rails['object_store']['enabled'] = true
@@ -25,14 +25,14 @@ gitlab_rails['object_store']['connection'] = {
 'aws_signature_version' => 4
 }
 
-gitlab_rails['object_store']['objects']['artifacts']['bucket'] = "gl-artifacts-dor"
-gitlab_rails['object_store']['objects']['external_diffs']['bucket'] = "gl-external-diffs-dor"
-gitlab_rails['object_store']['objects']['uploads']['bucket'] = "gl-uploads-dor"
-gitlab_rails['object_store']['objects']['lfs']['bucket'] = "gl-lfs-dor"
-gitlab_rails['object_store']['objects']['packages']['bucket'] = "gl-packages-dor"
-gitlab_rails['object_store']['objects']['dependency_proxy']['bucket'] = "gl-dependency-proxy-dor"
-gitlab_rails['object_store']['objects']['terraform_state']['bucket'] = "gl-terraform-dor"
-gitlab_rails['object_store']['objects']['ci_secure_files']['bucket'] = "gl-ci-secure-files-dor"
+gitlab_rails['object_store']['objects']['artifacts']['bucket'] = "gl-artifacts-<user>"
+gitlab_rails['object_store']['objects']['external_diffs']['bucket'] = "gl-external-diffs-<user>"
+gitlab_rails['object_store']['objects']['uploads']['bucket'] = "gl-uploads-<user>"
+gitlab_rails['object_store']['objects']['lfs']['bucket'] = "gl-lfs-<user>"
+gitlab_rails['object_store']['objects']['packages']['bucket'] = "gl-packages-<user>"
+gitlab_rails['object_store']['objects']['dependency_proxy']['bucket'] = "gl-dependency-proxy-<user>"
+gitlab_rails['object_store']['objects']['terraform_state']['bucket'] = "gl-terraform-<user>"
+gitlab_rails['object_store']['objects']['ci_secure_files']['bucket'] = "gl-ci-secure-files-<user>"
 gitlab_rails['backup_upload_connection'] = {
 'provider' => 'AWS',
 'region' => 'eu-west-1',
@@ -41,4 +41,4 @@ gitlab_rails['backup_upload_connection'] = {
 }
 gitlab_rails['backup_upload_remote_directory'] = 'gl-backups-dor';
 
-external_url "http://3.249.192.170"
+external_url "http://<ec2-ip>"
